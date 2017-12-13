873a029e7b8f913118b3b4269303fc6eacfff444
duodecimo <duodecimo@gmail.com>
2017-12-13 03:58:01 -0200

    display commands on image

--- project01/python3/ipWebcamCv2Controller.py
+++ project01/python3/ipWebcamCv2Controller.py
@@ -29,6 +29,7 @@ except:
   exit(1)
 
 bytes = bytes()
+msgtext = ''
 while True:
     bytes += stream.read(1024)
     a = bytes.find(b'\xff\xd8')
@@ -37,24 +38,31 @@ while True:
         jpg = bytes[a:b+2]
         bytes= bytes[b+2:]
         img = cv2.imdecode(np.fromstring(jpg,     dtype=np.uint8),cv2.IMREAD_COLOR)
+        cv2.putText(img, msgtext, (230, 50), cv2.FONT_HERSHEY_TRIPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+
         cv2.imshow('img',img)
         retval = np.int16(cv2.waitKey(1))
         if retval != -1:
           if retval ==27:
             exit(0)
           elif retval == -174:
-           print('FOWARD');
-           placeCall('FOWARD');
+           print('FOWARD')
+           placeCall('FOWARD')
+           msgtext = 'FOWARD'
           elif retval == -172:
-           print('BACK');
-           placeCall('BACK');
+           print('BACK')
+           placeCall('BACK')
+           msgtext = 'BACK'
           elif retval == -175:
-           print('LEFT');
-           placeCall('LEFT');
+           print('LEFT')
+           placeCall('LEFT')
+           msgtext = 'LEFT'
           elif retval == -173:
-           print('RIGH');
-           placeCall('RIGHT');
+           print('RIGH')
+           placeCall('RIGHT')
+           msgtext = 'RIGHT'
           else:
+            msgtext = 'UNKNOWN'
             print('unknown command, key: ', retval, ' type: ', type(retval))
 
 
