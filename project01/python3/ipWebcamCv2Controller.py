33f05f67f71c659afaeb0bbe2718595f290965d3
duodecimo <duodecimo@gmail.com>
2017-12-13 08:00:03 -0200

    adding file capture

--- project01/python3/ipWebcamCv2Controller.py
+++ project01/python3/ipWebcamCv2Controller.py
@@ -2,6 +2,9 @@ import cv2
 import urllib.request
 import numpy as np
 import argparse
+import time
+from datetime import datetime
+import os
 
 def placeCall(cmd):
   if args.wu != None:
@@ -11,6 +14,7 @@ def placeCall(cmd):
 parser = argparse.ArgumentParser()
 parser.add_argument("iwu", help = 'ipWebcam URL, i.e. \'http://192.168.25.7:8080/video\'', nargs='?', default='http://192.168.25.7:8080/video')
 parser.add_argument("wu", help = 'Wemos D1 esp8266 URL', nargs='?', default=None)
+parser.add_argument("path", help = 'path to store captured images', nargs='?', default='capture')
 args = parser.parse_args()
 
 print('Commands arrow keys:')
@@ -19,7 +23,11 @@ print('down -> Back')
 print('left -> Left')
 print('right-> Right')
 print('other:')
+print('s  -> stop')
+print('c  -> toggle cature')
 print('Esc-> exit')
+
+start_time = time.time()
 
 try:
   stream=urllib.request.urlopen(args.iwu)
@@ -29,41 +37,73 @@ except:
   exit(1)
 
 bytes = bytes()
-msgtext = ''
+command = ''
+is_capturing = False
+#lets make sure the path exists!
+if not os.access(args.path, os.F_OK):
+  os.makedirs(args.path)
+commands = [up, down, left, right, stop]
+
 while True:
     bytes += stream.read(1024)
     a = bytes.find(b'\xff\xd8')
     b = bytes.find(b'\xff\xd9')
+    # if there is at least one frame buffered
     if a!=-1 and b!=-1:
-        jpg = bytes[a:b+2]
-        bytes= bytes[b+2:]
-        img = cv2.imdecode(np.fromstring(jpg,     dtype=np.uint8),cv2.IMREAD_COLOR)
-        cv2.putText(img, msgtext, (230, 50), cv2.FONT_HERSHEY_TRIPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+      # get the frame bytes to form an image
+      jpg = bytes[a:b+2]
+      # keep the rest in the bytes buffer
+      bytes= bytes[b+2:]
+      img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+        
+      if is_capturing:
+        # check if it is time to save frame to a file
+        elapsed_time = time.time() - start_time
+        # save image each 1 second
+        if elapsed_time > 1 and command in commands:
+          timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+          timestamp = timestamp + '_' + command + '.jpg'
+          image_filename = os.path.join(args.path, timestamp)
+          # save the image
+          cv2.imwrite(image_filename, img)
+          #restart the timer
+          start_time = time.time()
+        
+      cv2.putText(img, command, (230, 50), cv2.FONT_HERSHEY_TRIPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+      if is_capturing:
+        cv2.putText(img, 'Capturing', (230, 450), cv2.FONT_HERSHEY_TRIPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
 
-        cv2.imshow('img',img)
-        retval = np.int16(cv2.waitKey(1))
-        if retval != -1:
-          if retval ==27:
-            exit(0)
-          elif retval == -174:
-           print('FOWARD')
-           placeCall('FOWARD')
-           msgtext = 'FOWARD'
-          elif retval == -172:
-           print('BACK')
-           placeCall('BACK')
-           msgtext = 'BACK'
-          elif retval == -175:
-           print('LEFT')
-           placeCall('LEFT')
-           msgtext = 'LEFT'
-          elif retval == -173:
-           print('RIGH')
-           placeCall('RIGHT')
-           msgtext = 'RIGHT'
-          else:
-            msgtext = 'UNKNOWN'
-            print('unknown command, key: ', retval, ' type: ', type(retval))
+      cv2.imshow('img',img)
+      retval = np.int16(cv2.waitKey(1))
+      if retval != -1:
+        if retval ==27:
+          exit(0)
+        elif retval == -174:
+         print('FOWARD')
+         placeCall('FOWARD')
+         command = 'FOWARD'
+        elif retval == -172:
+         print('BACK')
+         placeCall('BACK')
+         command = 'BACK'
+        elif retval == -175:
+         print('LEFT')
+         placeCall('LEFT')
+         command = 'LEFT'
+        elif retval == -173:
+         print('RIGH')
+         placeCall('RIGHT')
+         command = 'RIGHT'
+        elif retval == 's' or retval == 'S':
+         print('STOP')
+         placeCall('STOP')
+         command = 'STOP'
+        elif retval == 'c' or retval == 'C':
+         print('toggle capture')
+         is_capturing = !is_capturing
+        else:
+          command = 'UNKNOWN'
+          print('unknown command, key: ', retval, ' type: ', type(retval))
 
 
 
